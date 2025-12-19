1582a57b5c0a5015ed51be16c4862c8468f74bec
AM-LP-29 <nangarea2017@gmail.com>
2025-12-19 14:37:28 +0530

    [feat]: add updateStatus, deleteStatus, reloadStatus functionalities

--- TeamPlus/src/context/StatusContext.tsx
+++ TeamPlus/src/context/StatusContext.tsx
@@ -1,38 +1,73 @@
-import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Status } from "../types/status";
 
 interface StatusContextType {
   statuses: Status[];
   addStatus: (status: Status) => void;
+  deleteStatus: (id: string) => void;
+  updateStatus: (status: Status) => void;
+  reloadStatuses: () => Promise<void>;
 }
 
 const StatusContext = createContext<StatusContextType | undefined>(undefined);
 
 const STORAGE_KEY = "TEAMPLUS_STATUSES";
 
-export function StatusProvider({children}: {children: ReactNode}){
+export function StatusProvider({ children }: { children: ReactNode }) {
   const [statuses, setStatuses] = useState<Status[]>([]);
 
   useEffect(() => {
-    loadStatuses();
+    (async () => {
+      try {
+        const data = await AsyncStorage.getItem(STORAGE_KEY);
+        if (data) setStatuses(JSON.parse(data));
+      } catch (e) {
+        console.error("Load failed", e);
+      }
+    })();
   }, []);
 
-  const loadStatuses = async () => {
-    const data = await AsyncStorage.getItem(STORAGE_KEY);
-    if (data) {
-      setStatuses(JSON.parse(data));
+  useEffect(() => {
+    AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(statuses));
+  }, [statuses]);
+
+  const addStatus = (status: Status) => {
+    setStatuses((prev) => [status, ...prev]);
+  };
+
+  const deleteStatus = (id: string) => {
+    setStatuses((prev) => prev.filter((s) => s.id !== id));
+  };
+
+  const updateStatus = (updated: Status) => {
+    setStatuses((prev) =>
+      prev.map((s) => (s.id === updated.id ? updated : s))
+    );
+  };
+
+  const reloadStatuses = async() => {
+    try{
+      const data = await AsyncStorage.getItem(STORAGE_KEY);
+      if(data){
+        setStatuses(JSON.parse(data));
+      }
+    }catch(e){
+      console.error("reload failed ", e);
+      
     }
-  };
+  }
 
-  const addStatus = async (status: Status) => {
-    const updated = [status, ...statuses];
-    setStatuses(updated);
-    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(updated));
-  };
-
-  return(
-    <StatusContext.Provider value={{statuses, addStatus}}>
+  return (
+    <StatusContext.Provider
+      value={{statuses, addStatus, deleteStatus, updateStatus, reloadStatuses}}
+    >
       {children}
     </StatusContext.Provider>
   )
